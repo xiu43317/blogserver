3e7d16654155bbbc1edeb97056f6617ded5fda2e
Rock <xiu43317@gmail.com>
2020-11-23 09:22:56 +0800

    init

---
+++ routes/apis.js
@@ -0,0 +1,150 @@
+require('../lib/db');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Blog = mongoose.model('Blog');
+var Comment = mongoose.model('Comment');
+var Admin = mongoose.model('Admin');
+
+//使用者登入確認
+router.post('/admin', function (req, res, next) {
+    Admin.find(function (err, admins) {
+        if (err) {
+            res.send("Failure");
+        } else {
+            if(admins[0].Username === req.body.Username && admins[0].Password === req.body.Password){
+                res.send("Success");
+                console.log(req.body.Username);
+                console.log(req.body.Password);
+            }else{
+                res.send("Failure");
+                console.log('Failure');
+            }
+        }
+    });
+});
+
+//顯示全部文章
+router.get('/show', function(req, res, next) {
+    res.locals.username = req.session.name;
+    res.locals.authenticated = req.session.logined;
+    Blog.find(function(err,blogs,count){
+        if(err){
+            res.send(err);
+        }else{
+            res.send(blogs.reverse());
+        }
+    });
+  });
+
+/* 使用者刪除文章功能. */
+router.get('/delete/:id', function (req, res, next) {
+    Blog.deleteOne({ _id: req.params.id }, function (err) {
+        if (err) {
+            console.log('Fail to delete article');
+            res.send("Failure");
+        } else {
+            console.log('Done');
+            res.send("Success");
+        }
+    });
+    Comment.deleteMany({ MessageID: req.params.id }, function (err) {
+        if (err) {
+            console.log('Fail to delete message');
+        } else {
+            console.log('Message Delete');
+        }
+    });
+});
+/* 使用者新增文章功能. */
+router.post('/add', function (req, res, next) {
+    new Blog({
+        Username: req.body.Name,
+        Article: req.body.Content,
+        CreateDate: Date.now()
+    }).save(function (err) {
+        if (err) {
+            console.log('Fail to save to DB');
+            res.send('Failure');
+            return;
+        }
+        console.log('Save to DB');
+        res.send('Success');
+    });
+});
+
+/* 使用者更新文章功能. */
+router.post('/update/:id', function (req, res, next) {
+    if (!req.params.id) {
+        res.send("Failure");
+        console.log("Failure");
+        return;
+    }
+    Blog.updateOne({ _id: req.params.id },{$set:{ Username: req.body.Title, Article: req.body.Content}}, function (err) {
+        if (err) {
+            console.log('Fail to update article');
+            res.send("Failure");
+        } else {
+            console.log('Done');
+            res.send("Success");
+        }
+    });
+});
+
+// 搜尋留言
+router.get('/message/:id', function(req, res, next){
+        if (!req.params.id) {
+            res.send('Failure');
+            return;
+        }
+        Comment.find({ MessageID: req.params.id }, function( err, comments, count ){
+        if (err){
+            res.send(err);
+        }else{
+            res.send(comments.reverse());
+        }
+      });
+});
+
+/* 文章留言功能. */
+router.post('/comment/:id', function (req, res, next) {
+    if (!req.params.id) {
+        res.redirect('/');
+        return;
+    }
+    new Comment({
+        Vistor: req.body.Vistor,
+        Comment: req.body.Comment,
+        MessageID: req.params.id,
+        CreateDate: Date.now()
+    }).save(function (err) {
+        if (err) {
+            console.log('Fail to save DB');
+            res.send('Failure');
+            return;
+        }
+        console.log('Save to DB');
+        res.send('Success');
+    });
+});
+
+//留言刪除功能
+router.get('/deleteMessage/:id', function (req, res, next) {
+    if (!req.params.id) {
+        console.log('Failure');
+        res.send('Failure');
+        return
+    }
+    Comment.remove({ _id: req.params.id }, function (err) {
+        if (err) {
+            console.log('Fail to delete comment');
+            res.send('Failure');
+        } else {
+            console.log('Done');
+            res.send('Success');
+        }
+    });
+})
+
+module.exports = router;
+
